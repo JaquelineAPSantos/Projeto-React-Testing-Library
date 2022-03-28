f6e7c53dec0465876ea90e80fd022bc0486a3406
Jaqueline Araújo Pinheiro dos Santos <jaque.aps@hotmail.com>
2022-03-28 13:59:15 -0300

    Desenvolvendo o Primeiro Requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente App.js', () => {
+  test('O topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /favorite pokémons/i })).toBeInTheDocument();
+  });
+  test('O link home foi renderizado', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+  });
+});
