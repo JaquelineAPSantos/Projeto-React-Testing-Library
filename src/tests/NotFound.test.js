07acddb0fca2fa261d140e84c94dc2faea8cd804
Jaqueline Araújo Pinheiro dos Santos <jaque.aps@hotmail.com>
2022-03-29 15:26:23 -0300

    Requisito 04 Concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente <NotFound.js>', () => {
+  test('Teste se página contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+    const textNotFound = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+    expect(textNotFound).toBeInTheDocument();
+  });
+  test('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
